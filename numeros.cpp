65b00f273f5501b509b3dbd18d372d596de38976
ka.suarez7@gmail.com <ka.suarez7@gmail.com>
2018-07-06 12:03:51 -0500

    version mejorada

--- numeros.cpp
+++ numeros.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 #include <cstdlib>
 
-
 using std::cout;
 using std::endl;
 
-
-int main(){
+int main()
+{
   int i, numeroal;
-  for(i=0;i<1000;i++){
+  int cont=0;
+ 
+  for(i=0;i<1000;i++)
+   {
     numeroal = rand() % 100 + 1;
-    cout << i << " " << numeroal << endl;  
-    if (numeroal%2==0 && i<89)
+    //cout << i << " " << numeroal << endl;  
+    if (numeroal%2==0 && numeroal<89)
 	{
-	 cout  << " y los primeros 25 numeros son" << numeroal;
-	}	
-    }	
+	 cont=i;
+           if(cont<25)
+	    {
+	     cout << numeroal << endl;
+            }
+	    else
+	    {
+             break;     
+            }	
+         }
+    
+   }	
 
   return 0;
 }
